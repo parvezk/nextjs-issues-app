84f91289ce93c94199685adc8e570d86f4aac61f
Parvez Kose <parvez.kosex@gmail.com>
2025-01-08 13:03:00 -0500

    add env

--- src/lib/urqlClient.ts
+++ src/lib/urqlClient.ts
@@ -1,7 +1,10 @@
 import { createClient, cacheExchange, fetchExchange } from "urql";
 
 const urqlClient = createClient({
-  url: "http://localhost:3000/api/graphql",
+  url:
+    process.env.NEXT_PUBLIC_GRAPHQL_API_URL ||
+    "http://localhost:3000/api/graphql", // Fallback for local development
+
   exchanges: [cacheExchange, fetchExchange],
 });
 
