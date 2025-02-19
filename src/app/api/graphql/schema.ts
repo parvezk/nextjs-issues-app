dbef028fe1c8281a52b10da561abfdec70ef6aaf
Parvez Kose <parvez.kosex@gmail.com>
2025-02-19 14:20:18 -0500

    move schema

--- src/app/api/graphql/schema.ts
+++ src/app/api/graphql/schema.ts
@@ -1,5 +1,27 @@
 const schema = `#graphql
+ type User {
+      id: String!
+      email: String!
+      createdAt: String!
+    }
 
+  type Issue {
+    id: String!
+    name: String!
+    userId: String!
+    content: String!
+    status: String!
+    createdAt: String!
+  }
+
+  type Query {
+    users: [User!]!
+    issuesForUser(email: String!): [Issue!]!
+  }
+
+  type Mutation {
+    updateIssueStatus(id: String!, status: String!): Issue!
+  }
 
 `;
 
